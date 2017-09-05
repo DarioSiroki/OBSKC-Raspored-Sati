e914b57489a5637914f1958789405643a186ba46
DarioSiroki <30674077+DarioSiroki@users.noreply.github.com>
2017-09-05 09:58:25 +0200

    =)

--- js/ucenik.js
+++ js/ucenik.js
@@ -91,152 +91,152 @@ function ucenik(){
  //						Prikazivanje rasporeda
 if (exeCookie) {getRaspored();}
 
- 			function getRaspored()
- 			{
-				for (var i = razredi.length - 1; i >= 0; i--) // Loopanje kroz ucenike
-				{if (input!=="") { // Fixanje buga zbog kojeg su se petlje vrtile dok nije bilo inputa 		   				
-					   		if(input==razredi[i]) //Ako je razred unesen prikazi raspored (i isfiltriraj prije)
-					   			{
-					   			// Ponedjeljak
-					   			var text="";
-					   			for (y = 1; y < 8; y++){ //loopanje kroz stupce
+function getRaspored()
+{
+for (var i = razredi.length - 1; i >= 0; i--) // Loopanje kroz ucenike
+{if (input!=="") { // Fixanje buga zbog kojeg su se petlje vrtile dok nije bilo inputa 		   				
+	   		if(input==razredi[i]) //Ako je razred unesen prikazi raspored (i isfiltriraj prije)
+	   			{
+	   			// Ponedjeljak
+	   			var text="";
+	   			for (y = 1; y < 8; y++){ //loopanje kroz stupce
 
-					   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
-						   			text+="<th>"+y+"</th>"+"<tr>";
-						   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
-						   				
-						   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
-						   				{
-						   					
-						   					if(provjera==0){ //uvjet za provjeru 
-						   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
-						   				provjera++;
-						   				}
-						   						else{
-						   							text += "/" + data[x][0];
-						   						}
-
-						   			}
+	   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
+		   			text+="<th>"+y+"</th>"+"<tr>";
+		   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
+		   				
+		   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
+		   				{
+		   					
+		   					if(provjera==0){ //uvjet za provjeru 
+		   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
+		   				provjera++;
+		   				}
+		   						else{
+		   							text += "/" + data[x][0];
 		   						}
-		   						text+="</td></tr>";
-		   					}
-										document.getElementById('pon').innerHTML=text;
 
-					   			// Utorak
-					   			var text="";
-					   			for (y = 8; y < 15; y++){ //loopanje kroz stupce
+		   			}
+					}
+					text+="</td></tr>";
+				}
+						document.getElementById('pon').innerHTML=text;
 
-					   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
-						   			text+="<th>"+(y-7)+"</th>"+"<tr>";
-						   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
-						   				
-						   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
-						   				{
-						   					
-						   					if(provjera==0){ //uvjet za provjeru 
-						   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
-						   				provjera++;
-						   				}
-						   						else{
-						   							text += "/" + data[x][0];
-						   						}
+	   			// Utorak
+	   			var text="";
+	   			for (y = 8; y < 15; y++){ //loopanje kroz stupce
 
-						   			}
+	   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
+		   			text+="<th>"+(y-7)+"</th>"+"<tr>";
+		   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
+		   				
+		   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
+		   				{
+		   					
+		   					if(provjera==0){ //uvjet za provjeru 
+		   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
+		   				provjera++;
+		   				}
+		   						else{
+		   							text += "/" + data[x][0];
 		   						}
-		   						text+="</td></tr>";
-		   					}
-										document.getElementById('uto').innerHTML=text;
 
-					   			// Srijeda
-					   			var text="";
-					   			for (y = 15; y < 22; y++){ //loopanje kroz stupce
+		   			}
+					}
+					text+="</td></tr>";
+				}
+						document.getElementById('uto').innerHTML=text;
 
-					   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
-						   			text+="<th>"+(y-14)+"</th>"+"<tr>";
-						   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
-						   				
-						   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
-						   				{
-						   					
-						   					if(provjera==0){ //uvjet za provjeru 
-						   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
-						   				provjera++;
-						   				}
-						   						else{
-						   							text += "/" + data[x][0];
-						   						}
+	   			// Srijeda
+	   			var text="";
+	   			for (y = 15; y < 22; y++){ //loopanje kroz stupce
 
-						   			}
+	   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
+		   			text+="<th>"+(y-14)+"</th>"+"<tr>";
+		   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
+		   				
+		   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
+		   				{
+		   					
+		   					if(provjera==0){ //uvjet za provjeru 
+		   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
+		   				provjera++;
+		   				}
+		   						else{
+		   							text += "/" + data[x][0];
 		   						}
-		   						text+="</td></tr>";
-		   					}
-										document.getElementById('sri').innerHTML=text;
 
-					   			// Cetvrtak
-					   			var text="";
-					   			for (y = 22; y < 29; y++){ //loopanje kroz stupce
+		   			}
+					}
+					text+="</td></tr>";
+				}
+						document.getElementById('sri').innerHTML=text;
 
-					   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
-						   			text+="<th>"+(y-21)+"</th>"+"<tr>";
-						   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
-						   				
-						   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
-						   				{
-						   					
-						   					if(provjera==0){ //uvjet za provjeru 
-						   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
-						   				provjera++;
-						   				}
-						   						else{
-						   							text += "/" + data[x][0];
-						   						}
+	   			// Cetvrtak
+	   			var text="";
+	   			for (y = 22; y < 29; y++){ //loopanje kroz stupce
 
-						   			}
+	   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
+		   			text+="<th>"+(y-21)+"</th>"+"<tr>";
+		   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
+		   				
+		   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
+		   				{
+		   					
+		   					if(provjera==0){ //uvjet za provjeru 
+		   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
+		   				provjera++;
+		   				}
+		   						else{
+		   							text += "/" + data[x][0];
 		   						}
-		   						text+="</td></tr>";
-		   					}
-										document.getElementById('cet').innerHTML=text;
+
+		   			}
+					}
+					text+="</td></tr>";
+				}
+						document.getElementById('cet').innerHTML=text;
 
 
-					   			// Petak
-					   			var text="";
-					   			for (y = 29; y < 36; y++){ //loopanje kroz stupce
+	   			// Petak
+	   			var text="";
+	   			for (y = 29; y < 36; y++){ //loopanje kroz stupce
 
-					   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
-						   			text+="<th>"+(y-28)+"</th>"+"<tr>";
-						   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
-						   				
-						   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
-						   				{
-						   					
-						   					if(provjera==0){ //uvjet za provjeru 
-						   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
-						   				provjera++;
-						   				}
-						   						else{
-						   							text += "/" + data[x][0];
-						   						}
-
-						   			}
+	   			  var provjera = 0;	//varijabla za provjeru dijeli li se razred na grupe
+		   			text+="<th>"+(y-28)+"</th>"+"<tr>";
+		   			for (x = 2; x < data.length; x++){ //loopanje kroz redove		   				
+		   				
+		   				if(data[x][y]==razredi[i]) //ako je pronadjen raz 
+		   				{
+		   					
+		   					if(provjera==0){ //uvjet za provjeru 
+		   				text +=  "<td>" + data[x][0];//ispisi prvi stupac
+		   				provjera++;
+		   				}
+		   						else{
+		   							text += "/" + data[x][0];
 		   						}
-		   						text+="</td></tr>";
-		   					}
-										document.getElementById('pet').innerHTML=text; 	
-								
-						// Nakon dohvacanja fixevi		
-						var d = new Date();
-						var n = d.getDay();
-						if(n>5){n=1}
-							$( "#accordion" ).accordion({
-							  active:n-1, //Otvori tab koji je dan
-							})
-					   					document.activeElement.blur(); // Fix za bug tipkovnice koja ostane otvorena
-					   					document.getElementById("datum").innerHTML="Raspored za: " + data[0][0]+", razred:"+input;// Prikaze raspored uz datum, update
-					   					document.getElementById("zapamti").style.visibility="visible";
-					   					document.getElementById("ocisti").style.visibility="visible";
-								}}
-							}
-						}
+
+		   			}
+					}
+					text+="</td></tr>";
+				}
+						document.getElementById('pet').innerHTML=text; 	
+				
+		// Nakon dohvacanja fixevi		
+		var d = new Date();
+		var n = d.getDay();
+		if(n>5){n=1}
+			$( "#accordion" ).accordion({
+			  active:n-1, //Otvori tab koji je dan
+			})
+	   					document.activeElement.blur(); // Fix za bug tipkovnice koja ostane otvorena
+	   					document.getElementById("datum").innerHTML="Raspored za: " + data[0][0]+", razred:"+input;// Prikaze raspored uz datum, update
+	   					document.getElementById("zapamti").style.visibility="visible";
+	   					document.getElementById("ocisti").style.visibility="visible";
+				}}
+			}
+		}
 
 
 							
