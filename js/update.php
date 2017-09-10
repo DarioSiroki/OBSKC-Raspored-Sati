fb88f2f44bccabda2975a7b3fc62909daa0a7c47
DarioSiroki <30674077+DarioSiroki@users.noreply.github.com>
2017-09-10 09:23:12 +0200

    asd

--- js/update.php
+++ js/update.php
@@ -1,7 +1,7 @@
 <?php
 $myFile = "B.json";
 $fh = fopen($myFile, 'w') or die("can't open file");
-$stringData = $POST["data"];
+$stringData = $_POST["data"];
 fwrite($fh, $stringData);
 fclose($fh);
 ?>
