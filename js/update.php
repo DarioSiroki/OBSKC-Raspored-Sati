6728bdf0b0a43b8994ab2b36d3e515426d21fe75
DarioSiroki <30674077+DarioSiroki@users.noreply.github.com>
2017-09-10 10:18:22 +0200

    asd

--- js/update.php
+++ js/update.php
@@ -1,7 +1,9 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
-header('Access-Control-Max-Age: 1000');
+header('Access-Control-Max-Age: 1000');?>
+
+<?php
 $myFile = "B.json";
 $fh = fopen($myFile, 'w') or die("can't open file");
 $stringData = $_POST["data"];
