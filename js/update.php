c2d3999b5edd2b88fb8a219a18cb2617030fe04b
DarioSiroki <30674077+DarioSiroki@users.noreply.github.com>
2017-09-10 10:31:30 +0200

    asd

--- js/update.php
+++ js/update.php
@@ -1,9 +1,4 @@
 <?php
-header('content-type: application/json; charset=utf-8');
-header("access-control-allow-origin: *");
-?>
-
-<?php
 $myFile = "B.json";
 $fh = fopen($myFile, 'w') or die("can't open file");
 $stringData = $_POST["data"];
