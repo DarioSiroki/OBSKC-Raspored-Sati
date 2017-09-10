af7583d14e42001d85e28143161955a858af4f34
DarioSiroki <30674077+DarioSiroki@users.noreply.github.com>
2017-09-10 10:49:33 +0200

    fj

--- js/update.php
+++ js/update.php
@@ -1,13 +1,6 @@
 <?php
-switch ($_SERVER['HTTP_ORIGIN']) {
-    case 'http://from.com': case 'https://from.com':
-    header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-    break;
-}?>
-
+header('Access-Control-Allow-Origin: *'); 
+?>
 
 <?php
 $myFile = "B.json";
