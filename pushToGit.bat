07a96f3fe9bbb495a37bac0c80e848e762d858df
Dario Siroki <dario.siroki@gmail.com>
2018-03-30 15:03:06 +0200

    auto-update

--- pushToGit.bat
+++ pushToGit.bat
@@ -1,6 +1,9 @@
 D:
+cd D:\programiranje\USBWebserver v8.6\root\OBSKC-Raspored-Sati\converter\src
+node download.js
+node convert.js
 cd D:\programiranje\USBWebserver v8.6\root\OBSKC-Raspored-Sati
 git add .
-git commit -m "novi raspored"
+git commit -m "auto-update"
 git push origin master
 pause
