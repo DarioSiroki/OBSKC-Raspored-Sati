0ca16946a84d350d6bc338ee5074447fc36c2443
Dario Siroki <dario.siroki@gmail.com>
2018-09-02 14:49:00 +0200

    auto-update

--- converter/update.bat
+++ converter/update.bat
@@ -1,7 +1,7 @@
 D:
 cd D:\programiranje\USBWebserver v8.6\root\OBSKC-Raspored-Sati\converter\src
-node download.js
-node convert.js
+
+
 cd D:\programiranje\USBWebserver v8.6\root\OBSKC-Raspored-Sati
 git add .
 git status
