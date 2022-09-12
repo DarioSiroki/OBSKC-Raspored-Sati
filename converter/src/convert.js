e0cfc709587858ab190166bd1f66ce36d65b30bb
DarioSiroki <dario.siroki@gmail.com>
2022-09-12 21:56:35 +0200

    nepotreban setTimeout

--- converter/src/convert.js
+++ converter/src/convert.js
@@ -167,152 +167,149 @@ let formatName = (input) => {
 // skeniranje foldera
 paths = [];
 fs.readdirSync(scanDir).forEach((file) => {
-  fs.stat(scanDir + file, (err, stats) => {
-    let seconds = (new Date().getTime() - stats.mtime) / 1000;
-    if (seconds < 172800) {
-      //172800 = 2 dana, ako je file modified prije vise od 2 dana ne konvertiraj ga
-      paths.push(file);
-    }
-  });
+  const stats = fs.statSync(scanDir + file);
+  let seconds = (new Date().getTime() - stats.mtime) / 1000;
+  //172800 = 2 dana, ako je file modified prije vise od 2 dana ne konvertiraj ga
+  if (seconds < 172800) {
+    paths.push(file);
+  }
 });
+
 // izbacivanje stavki koje nisu xlsx
+paths = paths.filter((word) => word.toUpperCase().indexOf("XLSX") !== -1);
 
-setTimeout(() => {
-  paths = paths.filter((word) => word.toUpperCase().indexOf("XLSX") !== -1);
-  for (let i = 0; i < paths.length; i++) {
-    var workbookRead;
-    // citanje xlsxa
-    try {
-      workbookRead = XLSX.readFile(scanDir + paths[i], {
-        cellStyles: true,
-        cellHTML: false,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+for (let i = 0; i < paths.length; i++) {
+  var workbookRead;
+  // citanje xlsxa
+  try {
+    workbookRead = XLSX.readFile(scanDir + paths[i], {
+      cellStyles: true,
+      cellHTML: false,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 
-    var workbook = workbookRead.Sheets[workbookRead.Props.SheetNames[0]];
+  var workbook = workbookRead.Sheets[workbookRead.Props.SheetNames[0]];
 
-    var final = [];
-    var info = {
-      smjena: workbook.A1.v.charAt(0),
-      datum: workbook.A2.v,
-      prijeposlije:
-        workbook.A1.v.search("POSLIJE") === -1 ? "PRIJE" : "POSLIJE",
-      napomena: "",
-    };
+  var final = [];
+  var info = {
+    smjena: workbook.A1.v.charAt(0),
+    datum: workbook.A2.v,
+    prijeposlije: workbook.A1.v.search("POSLIJE") === -1 ? "PRIJE" : "POSLIJE",
+    napomena: "",
+  };
 
-    final.push(info);
+  final.push(info);
 
-    // vrti kroz prvih 100 redaka
-    for (let ii = 4; ii < 100; ii++) {
-      if (typeof workbook["A" + ii] !== "undefined") {
-        let profesor = [];
-        profesor.push(workbook["A" + ii].v);
-        for (let iii = 0; iii < alphabet.length; iii++) {
-          let razred = workbook[alphabet[iii] + ii];
-          if (typeof razred == "undefined" || razred.v === "") {
-            // za prazne celije
-            profesor.push("");
-          } else {
-            let fill = {
-              name: razred.v,
-            };
+  // vrti kroz prvih 100 redaka
+  for (let ii = 4; ii < 100; ii++) {
+    if (typeof workbook["A" + ii] !== "undefined") {
+      let profesor = [];
+      profesor.push(workbook["A" + ii].v);
+      for (let iii = 0; iii < alphabet.length; iii++) {
+        let razred = workbook[alphabet[iii] + ii];
+        if (typeof razred == "undefined" || razred.v === "") {
+          // za prazne celije
+          profesor.push("");
+        } else {
+          let fill = {
+            name: razred.v,
+          };
 
-            if (razred.s.fill) {
-              // za pozadinsku boju
-              if (
-                razred.s.fill.fgColor &&
-                Object.keys(razred.s.fill.fgColor).length !== 0
-              ) {
-                // za pozadinsku boju (fgColor)
-                if (razred.s.fill.fgColor.hasOwnProperty("tint")) {
-                  fill.bgColor = tintAndHexToHex(
-                    razred.s.fill.fgColor.rgb.length > 6
-                      ? razred.s.fill.fgColor.rgb.substring(2)
-                      : razred.s.fill.fgColor.rgb,
-                    razred.s.fill.fgColor.tint
-                  );
-                } else {
-                  if (razred.s.fill.fgColor.rgb !== "000000") {
-                    // .substring() pretvara excel value u hex value boje (excel koristi hex sa FF na pocetku stringa)
-                    if (razred.s.fill.fgColor.rgb.length > 6) {
-                      fill.bgColor = razred.s.fill.fgColor.rgb.substring(2);
-                    } else {
-                      fill.bgColor = razred.s.fill.fgColor.rgb;
-                    }
+          if (razred.s.fill) {
+            // za pozadinsku boju
+            if (
+              razred.s.fill.fgColor &&
+              Object.keys(razred.s.fill.fgColor).length !== 0
+            ) {
+              // za pozadinsku boju (fgColor)
+              if (razred.s.fill.fgColor.hasOwnProperty("tint")) {
+                fill.bgColor = tintAndHexToHex(
+                  razred.s.fill.fgColor.rgb.length > 6
+                    ? razred.s.fill.fgColor.rgb.substring(2)
+                    : razred.s.fill.fgColor.rgb,
+                  razred.s.fill.fgColor.tint
+                );
+              } else {
+                if (razred.s.fill.fgColor.rgb !== "000000") {
+                  // .substring() pretvara excel value u hex value boje (excel koristi hex sa FF na pocetku stringa)
+                  if (razred.s.fill.fgColor.rgb.length > 6) {
+                    fill.bgColor = razred.s.fill.fgColor.rgb.substring(2);
+                  } else {
+                    fill.bgColor = razred.s.fill.fgColor.rgb;
                   }
                 }
               }
             }
-
-            if (razred.s.font.color) {
-              // za boju fonta
-              if (
-                razred.s.font.color.rgb &&
-                Object.keys(razred.s.font.color.rgb).length !== 0
-              ) {
-                if (razred.s.font.color.rgb.length > 6) {
-                  fill.fontColor = razred.s.font.color.rgb.substring(2);
-                } else {
-                  fill.fontColor = razred.s.font.color.rgb;
-                }
-              }
-            }
-
-            if (razred.s.fill) {
-              // za patterne
-              if (razred.s.fill.patternType) {
-                if (razred.s.fill.patternType !== "solid") {
-                  fill.pattern = razred.s.fill.patternType;
-                }
-              }
-            }
-
-            profesor.push(fill);
           }
+
+          if (razred.s.font.color) {
+            // za boju fonta
+            if (
+              razred.s.font.color.rgb &&
+              Object.keys(razred.s.font.color.rgb).length !== 0
+            ) {
+              if (razred.s.font.color.rgb.length > 6) {
+                fill.fontColor = razred.s.font.color.rgb.substring(2);
+              } else {
+                fill.fontColor = razred.s.font.color.rgb;
+              }
+            }
+          }
+
+          if (razred.s.fill) {
+            // za patterne
+            if (razred.s.fill.patternType) {
+              if (razred.s.fill.patternType !== "solid") {
+                fill.pattern = razred.s.fill.patternType;
+              }
+            }
+          }
+
+          profesor.push(fill);
         }
-        final.push(profesor);
+      }
+      final.push(profesor);
+    }
+  }
+
+  // dezurni prof.
+  for (let i = final.length - 1; i >= 0; i--) {
+    for (let ii = 0; ii < final[i].length; ii++) {
+      if (final[i].indexOf("DEŽURNI PROFESORI") != -1) {
+        final[0].dezurni = final[i].filter(function (e) {
+          return e;
+        });
       }
     }
+  }
 
-    // dezurni prof.
-    for (let i = final.length - 1; i >= 0; i--) {
-      for (let ii = 0; ii < final[i].length; ii++) {
-        if (final[i].indexOf("DEŽURNI PROFESORI") != -1) {
-          final[0].dezurni = final[i].filter(function (e) {
-            return e;
-          });
-        }
+  fs.writeFile(
+    writeDir + formatName(paths[i]),
+    JSON.stringify(filter(final)),
+    function (err) {
+      if (err) {
+        return console.log(err);
       }
+      rmWeirdfiles();
+      console.log(paths[i] + " konvertiran");
     }
+  );
 
+  if (debug) {
     fs.writeFile(
-      writeDir + formatName(paths[i]),
-      JSON.stringify(filter(final)),
+      writeDir + "debug_" + formatName(paths[i]),
+      JSON.stringify(workbookRead),
       function (err) {
         if (err) {
           return console.log(err);
         }
-        rmWeirdfiles();
-        console.log(paths[i] + " konvertiran");
       }
     );
-
-    if (debug) {
-      fs.writeFile(
-        writeDir + "debug_" + formatName(paths[i]),
-        JSON.stringify(workbookRead),
-        function (err) {
-          if (err) {
-            return console.log(err);
-          }
-        }
-      );
-    }
   }
-  writeVersions();
-}, 5000);
+}
+writeVersions();
 /*
 const legendaUcionice = {
 	"FF99CC": "uč. 35",
